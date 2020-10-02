e6cfaab9f6c2d8ba25f746848ecc97527d91245e
Ethery <loic.meunier74@gmail.com>
2020-10-02 23:48:05 +0200

    Bases for UI.

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	public static T Instance { get { return s_instance; } }
+	public static bool IsInstanced { get { return s_instance != null; } }
+
+	private static T s_instance = null;
+	#region Private
+
+	protected virtual void Awake()
+	{
+		if (!IsInstanced)
+		{
+			s_instance = this as T;
+
+			if (transform.parent == null)
+			{
+				DontDestroyOnLoad(this);
+			}
+		}
+
+		if (s_instance != this)
+		{
+			Destroy(this);
+		}
+	}
+
+	protected virtual void OnDestroy()
+	{
+		if (s_instance == this)
+		{
+			s_instance = null;
+		}
+	}
+
+	#endregion
+}
---
+++ Assets/UIPage.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class UIPage : MonoBehaviour
+{
+	public static Dictionary<EPage,List<UIPage>> PagesRefs
+	{
+		get;
+		protected set;
+	}
+
+	public enum EPage
+	{
+		MainMenu,
+		Credits
+	}
+
+	public enum EState
+	{
+		Shown,
+		Hidden
+	}
+
+	public EPage Page;
+	public EState CurrentState
+	{
+		get;
+		private set;
+	}
+	private void Awake()
+	{
+		if (!PagesRefs.ContainsKey(Page))
+			PagesRefs.Add(Page, new List<UIPage>());
+		PagesRefs[Page].Add(this);
+	}
+
+	private void OnDestroy()
+	{
+		PagesRefs[Page].Remove(this);
+		if (PagesRefs[Page].Count <= 0)
+			PagesRefs.Remove(Page);
+	}
+	public static void Show(EPage aPage, bool aShow)
+	{
+		foreach(UIPage page in PagesRefs[aPage])
+		{
+			if(page.CurrentState == (aShow ? EState.Hidden:EState.Shown))
+			{
+				page.Show(aShow);
+			}
+		}
+	}
+
+	public void Show(bool aShow)
+	{
+		CurrentState = aShow ? EState.Shown : EState.Hidden;
+		gameObject.SetActive(aShow);
+	}
+}

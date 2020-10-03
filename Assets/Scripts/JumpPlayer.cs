b3678d479fb192e667d2a26b8019e0565b2967e2
Unknown <robin.flavigny@free.fr>
2020-10-03 12:32:12 +0200

    Added Player

---
+++ Assets/Scripts/JumpPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class JumpPlayer : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float forceSaut = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown("z"))
+        {
+            Debug.Log("Saut");
+            rb.AddForce(new Vector2(0, forceSaut), ForceMode2D.Impulse);    
+        }   
+    }
+}

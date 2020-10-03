7c09540b10eb8040ba02fdbe3328a05cc2957aaf
Unknown <robin.flavigny@free.fr>
2020-10-03 11:45:00 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/LevelRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelRotation : MonoBehaviour
+{
+    public float rotationSpeed;
+
+
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey("q"))
+        {
+            transform.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
+        }
+    }
+}

eed45b96c7b4aad0b3f07f0f85c655f9b7ccdfa2
Unknown <robin.flavigny@free.fr>
2020-10-04 11:28:33 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/EndObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class EndObject : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Fin du niveau !");
+        }
+    }
+}

b9098740ed903b31333123ae69ad6133303d4c8e
Unknown <robin.flavigny@free.fr>
2020-10-05 11:30:03 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/PositionEchelle.cs
+++ Assets/Scripts/PositionEchelle.cs
@@ -2,34 +2,19 @@
 
 public class PositionEchelle : MonoBehaviour
 {
-	public GameObject Echelle;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
+        {
+            GameManager.Player.canPositionEchelle = true;
+        }
+    }
 
-	private void Start()
-	{
-		//Echelle = GameObject.Find("Echelle");
-	}
-
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if (collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
-		{
-			GameManager.Player.canPositionEchelle = true;
-		}
-	}
-
-	private void OnTriggerExit2D(Collider2D collision)
-	{
-		if (collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
-		{
-			GameManager.Player.canPositionEchelle = false;
-		}
-	}
-
-	private void Update()
-	{
-		if (GameManager.Player.canPositionEchelle)
-		{
-			//transform.SetPositionAndRotation();
-		}
-	}
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
+        {
+            GameManager.Player.canPositionEchelle = false;
+        }
+    }
 }

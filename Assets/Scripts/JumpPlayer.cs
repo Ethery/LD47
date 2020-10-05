4d536fb645ba4b19d6a0886ee3b37b60e894e56d
Unknown <robin.flavigny@free.fr>
2020-10-05 16:33:12 +0200

    Update Ladder

--- Assets/Scripts/JumpPlayer.cs
+++ Assets/Scripts/JumpPlayer.cs
@@ -9,15 +9,18 @@ public class JumpPlayer : MonoBehaviour
 	public float forceSaut = 5;
 	public float MoveSpeed = 5f;
 	public float runMultiplicator = 2;
-	public bool canPositionEchelle = false;
-	public GameObject Echelle;
+    public bool canPositionEchelle => Place != null;
+	public EchelleObject Echelle;
+    public PositionEchelle Place;
 
 	[HideInInspector]
 	public bool CanMove = true;
+    public bool ClimbingToLadder;
 
-	public bool IsGrounded { get; private set; }
 
-	private void Awake()
+    public bool IsGrounded { get; private set; }
+
+    private void Awake()
 	{
 		CanMove = true;
 		GameManager.Player = this;
@@ -32,46 +35,82 @@ public class JumpPlayer : MonoBehaviour
 	// Update is called once per frame
 	private void Update()
 	{
-		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded && CanMove)
+        if(!ClimbingToLadder)
+            UpdateMovements();
+        else
+        {
+            if(CanMove)
+            {
+                //GRIMPER A L'ECHELLE
+            }
+        }
+
+        if (!CanMove)
+            return;
+
+        if (Echelle != null && Input.GetKeyDown(KeyCode.E))
 		{
-			IsGrounded = false;
-			rb.AddForce(transform.up * forceSaut, ForceMode2D.Impulse);
-		}
+            if(Echelle.isPlaced)
+            {
+                ClimbingToLadder = !ClimbingToLadder;
+            }
 
-		Vector3 ZeroRotVelocity = transform.InverseTransformVector(new Vector3(rb.velocity.x, rb.velocity.y, 0));
-
-		ZeroRotVelocity.x = 0;
-
-		if (Input.GetKey(KeyCode.D) && CanMove)
-		{
-			GetComponent<SpriteRenderer>().flipX = false;
-			ZeroRotVelocity.x = MoveSpeed;
-		}
-		if (Input.GetKey(KeyCode.Q) && CanMove)
-		{
-			GetComponent<SpriteRenderer>().flipX = true;
-			ZeroRotVelocity.x = -MoveSpeed;
-		}
-		if (Input.GetKey(KeyCode.LeftShift) && CanMove)
-		{
-			ZeroRotVelocity.x *= runMultiplicator;
-		}
-		float maxSpeed = MoveSpeed * runMultiplicator;
-		float lerpedSpeed = (Mathf.InverseLerp(-maxSpeed, maxSpeed, ZeroRotVelocity.x) * 2) - 1;
-		anim.SetFloat("HorizontalSpeed", lerpedSpeed);
-		anim.SetFloat("VerticalSpeed", ZeroRotVelocity.y);
-		rb.velocity = transform.TransformVector(ZeroRotVelocity);
-
-		Vector3 CenterOfPlanet = Vector3.zero;
-		Vector3 downDirection = (CenterOfPlanet - transform.position).normalized;
-
-		transform.rotation = Quaternion.LookRotation(Vector3.forward, -downDirection);
-		Physics2D.gravity = downDirection * 9.81f;
-
-		if (canPositionEchelle && Input.GetKeyDown(KeyCode.E) && CanMove)
-		{
+            if (Echelle.IsGrabbed && canPositionEchelle)
+            {
+                Echelle.transform.SetParent(Place.transform);
+                Echelle.isPlaced = true;
+            }
+            else
+            {
+                Echelle.IsGrabbed = !Echelle.IsGrabbed;
+                if (Echelle.IsGrabbed)
+                    Echelle.transform.SetParent(transform);
+                else
+                {
+                    Echelle.transform.SetParent(null);
+                }
+            }
 		}
 	}
+
+    public void UpdateMovements()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded && CanMove)
+        {
+            IsGrounded = false;
+            rb.AddForce(transform.up * forceSaut, ForceMode2D.Impulse);
+        }
+
+        Vector3 ZeroRotVelocity = transform.InverseTransformVector(new Vector3(rb.velocity.x, rb.velocity.y, 0));
+
+        ZeroRotVelocity.x = 0;
+
+        if (Input.GetKey(KeyCode.D) && CanMove)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            ZeroRotVelocity.x = MoveSpeed;
+        }
+        if (Input.GetKey(KeyCode.Q) && CanMove)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            ZeroRotVelocity.x = -MoveSpeed;
+        }
+        if (Input.GetKey(KeyCode.LeftShift) && CanMove)
+        {
+            ZeroRotVelocity.x *= runMultiplicator;
+        }
+        float maxSpeed = MoveSpeed * runMultiplicator;
+        float lerpedSpeed = (Mathf.InverseLerp(-maxSpeed, maxSpeed, ZeroRotVelocity.x) * 2) - 1;
+        anim.SetFloat("HorizontalSpeed", lerpedSpeed);
+        anim.SetFloat("VerticalSpeed", ZeroRotVelocity.y);
+        rb.velocity = transform.TransformVector(ZeroRotVelocity);
+
+        Vector3 CenterOfPlanet = Vector3.zero;
+        Vector3 downDirection = (CenterOfPlanet - transform.position).normalized;
+
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, -downDirection);
+        Physics2D.gravity = downDirection * 9.81f;
+    }
 
 	private void OnTriggerStay2D(Collider2D collision)
 	{

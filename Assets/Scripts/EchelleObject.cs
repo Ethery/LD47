3fd6de3ac0c3cb8ae4533fe7a31c2383ab52551f
Ethery <loic.meunier74@gmail.com>
2020-10-05 18:26:39 +0200

    fixes for puzzle 2

--- Assets/Scripts/EchelleObject.cs
+++ Assets/Scripts/EchelleObject.cs
@@ -4,40 +4,40 @@ public class EchelleObject : MonoBehaviour
 {
 	public Vector3 PositionOffset = new Vector3(0, 0, 0);
 	public Vector3 RotationOffset = new Vector3(0, 0, 0);
-    public bool isPlaced = false;
+	public bool IsPlaced = false;
 	public bool IsGrabbed = false;
 
 	private void Update()
 	{
-        if(IsGrabbed)
-        {
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
-        }
-        transform.localScale = Vector3.one;
-    }
+		if (IsGrabbed || IsPlaced)
+		{
+			transform.localPosition = Vector3.zero;
+			transform.localRotation = Quaternion.identity;
+		}
+		transform.localScale = Vector3.one;
+	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.attachedRigidbody != null && collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
-        {
-            GameManager.Player.Echelle = this;
-        }
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.attachedRigidbody != null && collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
-        {
-            GameManager.Player.Echelle = this;
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.attachedRigidbody != null && collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
+		{
+			GameManager.Player.Echelle = this;
+		}
+	}
 
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.attachedRigidbody != null && collision.attachedRigidbody.gameObject == GameManager.Player.gameObject)
+		{
+			GameManager.Player.Echelle = this;
+		}
+	}
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.attachedRigidbody != null && collision.attachedRigidbody.gameObject == GameManager.Player.gameObject && !IsGrabbed)
-        {
-            GameManager.Player.Echelle = null;
-        }
-    }
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.attachedRigidbody != null && collision.attachedRigidbody.gameObject == GameManager.Player.gameObject && !IsGrabbed)
+		{
+			GameManager.Player.Echelle = null;
+		}
+	}
 }
